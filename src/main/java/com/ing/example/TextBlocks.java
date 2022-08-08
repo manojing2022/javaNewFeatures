a979f4e071429d920e63470dce9b94fa453d60f5
manojing2022 <manoj.mallick@ing.com>
2022-08-09 01:22:56 +0200

    new way

--- src/main/java/com/ing/example/TextBlocks.java
+++ src/main/java/com/ing/example/TextBlocks.java
@@ -15,10 +15,16 @@ public class TextBlocks {
           }\
       """;
 
+  private static final String JSON_STRING_SPACE = """
+          |one          \s
+          |two                   
+      """;
+
   public static void main(String[] args) {
     System.out.println(JSON_STRING);
     System.out.println("=====================================");
     System.out.println(JSON_STRING_ESCAPE);
     System.out.println("=====================================");
+    System.out.println(JSON_STRING_SPACE);
   }
 }
