00ed9be17a9baba4969518ac26eacf95e869f797
manojing2022 <manoj.mallick@ing.com>
2022-08-09 01:17:26 +0200

    new way

---
+++ src/main/java/com/ing/example/TextBlocks.java
@@ -0,0 +1,24 @@
+package com.ing.example;
+
+public class TextBlocks {
+
+  private static final String JSON_STRING = """
+          {
+          "name" : "ing",
+          "website" : "https://www.ing.nl/"
+          }
+      """;
+  private static final String JSON_STRING_ESCAPE = """
+          {
+          "name" : "ing",
+          "website" : "https://www.ing.nl/"
+          }\
+      """;
+
+  public static void main(String[] args) {
+    System.out.println(JSON_STRING);
+    System.out.println("=====================================");
+    System.out.println(JSON_STRING_ESCAPE);
+    System.out.println("=====================================");
+  }
+}
