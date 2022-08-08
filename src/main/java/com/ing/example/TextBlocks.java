673b2d40e93249d19bd4d914bc761faf23b220ec
manojing2022 <manoj.mallick@ing.com>
2022-08-09 01:13:36 +0200

    old way

---
+++ src/main/java/com/ing/example/TextBlocks.java
@@ -0,0 +1,9 @@
+package com.ing.example;
+
+public class TextBlocks {
+  private static final String JSON_STRING = "{\r\n" + "\"name\" : \"ING\",\r\n" + "\"website\" : \"https://www.ing.nl/\"\r\n" + "}";
+
+  public static void main(String[] args) {
+    System.out.println(JSON_STRING);
+  }
+}
