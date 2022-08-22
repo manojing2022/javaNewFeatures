7938f2f4a47b265745ec3118e68633fb5cbd07fb
manojing2022 <manoj.mallick@ing.com>
2022-08-23 00:26:03 +0200

    interface with private

---
+++ src/main/java/com/ing/example/SampleInterface.java
@@ -0,0 +1,18 @@
+package com.ing.example;
+
+public interface SampleInterface {
+
+  Long createId();
+
+  void displayDetails();
+
+   void displayDetailsV2(){
+    //something
+  }
+
+   Long displayDetailsV3(){
+    //something
+    return 0L;
+  }
+
+}
